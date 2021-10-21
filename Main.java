fc0d5c83a93c09fa172650470e982ae4c054f5c9
Grofor Armas <goav98@gmail.com>
2021-10-21 19:45:03 +0000

    idk

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 /*
 * @author Grofor Armas
 */
-class Main {
+public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     System.out.println("La suma es: "+suma(2,2));
