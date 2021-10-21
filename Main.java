6280607ee051e719fefd44f88e0c5ff0ccd79602
GroforOctavioOc <>
2021-10-21 19:12:01 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+/*
+* @author Grofor Armas
+*/
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println("La suma es: "+suma(2,2));
+  }
+  //comentario con dos asteriscos
+  /**
+  * @param a  Primer entero de la ecuación
+  * @param b Segundo entero de la suma
+  *@return   La suma de los enteros: a más b
+  */
+
+  public static int suma(int a, int b){
+    return a+b;
+  }
+}
